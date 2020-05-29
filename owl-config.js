f23855e1ef249d7fc036390de67e0effca1897a5
Kiệt <kietteikdoi@gmail.com>
2020-05-29 20:49:33 +0700

    priceBuilding

--- owl-config.js
+++ owl-config.js
@@ -19,3 +19,24 @@ $(".owl-carousel").owlCarousel({
         },
     },
 });
+$(".owl-carousel-navlist").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2,
+        },
+        600: {
+            items: 3,
+        },
+        864: {
+            items: 4,
+        },
+        1000: {
+            items: 6,
+            loop: false,
+        },
+    },
+});
